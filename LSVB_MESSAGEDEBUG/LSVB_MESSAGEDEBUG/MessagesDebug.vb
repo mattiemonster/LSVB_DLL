e8180ec0a04c15f70c3c75830dcc90a7cdb8d08d
MattieMonster <mattiemonster1@hotmail.com>
2016-08-30 21:42:38 +0100

    MessagesDebug Development #1

---
+++ LSVB_MESSAGEDEBUG/LSVB_MESSAGEDEBUG/MessagesDebug.vb
@@ -0,0 +1,8 @@
+﻿Public Class MessagesDebug
+
+    Public Shared Function testMsg()
+        MsgBox("Test!", MsgBoxStyle.Information, "Testing!")
+        Return True
+    End Function
+
+End Class
---
+++ LSVB_MESSAGEDEBUG/LSVB_MESSAGEDEBUG/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LSVB_MESSAGEDEBUG")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("LSVB_MESSAGEDEBUG")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9c57a22d-d941-451c-bb5b-e331c34f48eb")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
